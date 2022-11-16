11e0a142666c1c6f76b098b4be760fdb57694599
yhe53 <yueninghe53@gmail.com>
2022-11-16 00:07:26 -0500

    Implement appbar with linear gradient color

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -11,9 +11,28 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Hello World"
-        )
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.pinkAccent,
+                Colors.purpleAccent,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: const Text(
+          "iShop",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
     );
   }
